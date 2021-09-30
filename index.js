18df9ff16dc2fbd9a9937e1db1b3c7cf6b64d032
FabioLima2603 <89948909+FabioLima2603@users.noreply.github.com>
2021-09-29 22:31:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ if(peso >=100){
 } else {
     console.log("Peso da peça inválido")
 }
+var Qtdepeça = window.prompt("Insira quantidade de peça");
 var entradadepeças = window.prompt("Insira uma peça");
 var ListaDePecas = [ListaDePecas = entradadepeças];
-console.log("Quantidade de peças: " + ListaDePecas)
+console.log("quantidade de pecas" + Qtdepeça);
 
 if(ListaDePecas.length <=10){
     console.log('As peças já estão cadastradas');
 } 
 
-for (var i = 0; i < ListaDePecas.length; i ++){
+for (var i = 0; i < Qtdepeça; i ++){
+   entradadepeças = window.prompt("Insira uma peça");
+   ListaDePecas = [ListaDePecas = entradadepeças];
+   console.log(ListaDePecas +'')
     var PecaAtual = ListaDePecas[i];
-    if(PecaAtual.length < 4) {
-        console.log(PecaAtual + ' Esta peça possui nome inferior a 4 caracteres! ')
+   
+ if(entradadepeças.length < 4) {
+      console.log(entradadepeças + ' Esta peça possui nome inferior a 4 caracteres! ')
     } else {
-        console.log(PecaAtual + ' Está peça podera! Ser Cadastrada. ')
+     console.log(entradadepeças + ' Está peça podera! Ser Cadastrada. ')
     }
 }
